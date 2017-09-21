88d9abdd7e42c455a7e0333a9136eae0cb65ed51
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2017-09-21 13:49:35 +0900

    Add root

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :genes
   resources :organisms
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'organisms#index'
 end
